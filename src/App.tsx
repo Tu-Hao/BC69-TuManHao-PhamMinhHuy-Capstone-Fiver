8060584eabf32aa44f07d108f2fdfc9660ff1c6c
Tu-hao <tumanhao99@gmail.com>
2024-09-11 23:56:55 +0700

    khởi tạo dự án capstone fiver. Đã cài redux toolkit,antd,axios,react-redux,react-router-dom và tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='bg-stone-500 text-lime-500 font-bold text-center text-5xl p-5'>Khởi tạo dự án capstone fiver</h1>
+
+    </>
+  )
+}
+
+export default App
